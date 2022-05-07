d8a8fec718c92331f83d98deecd62094906f0fb8
ksengland <karleenengland@erunbox.com>
2022-05-07 17:36:59 -0500

    Turned it into a party

--- pacmen.js
+++ pacmen.js
@@ -1,7 +1,7 @@
 let pos = 0;
 const pacArray = [
-  ['./images/PacMan1.png', './images/PacMan2.png'],
-  ['./images/PacMan3.png', './images/PacMan4.png'],
+  ['./images/PacMan1.png'],['./images/PacMan2.png'],
+  ['./images/PacMan3.png'], ['./images/PacMan4.png'],
 ];
 let direction = 0;
 const pacMen = []; // This array holds all the pacmen
@@ -14,17 +14,20 @@ function setToRandom(scale) {
   };
 }
 
+
 // Factory to make a PacMan at a random position with random velocity
 function makePac() {
+  
   // returns an object with random values scaled {x: 33, y: 21}
   let velocity = setToRandom(10); // {x:?, y:?}
   let position = setToRandom(200);
 
   // Add image to div id = game
+  const randomImage = pacArray[Math.floor(Math.random()*pacArray.length)];
   let game = document.getElementById('game');
   let newimg = document.createElement('img');
   newimg.style.position = 'absolute';
-  newimg.src = './images/PacMan1.png';
+  newimg.src = randomImage
   newimg.width = 100;
   newimg.style.left = position.x;
   newimg.style.top = position.y;
@@ -67,7 +70,7 @@ function makeOne() {
   pacMen.push(makePac()); // add a new PacMan
 }
 
-//don't change this line
-if (typeof module !== 'undefined') {
-  module.exports = { checkCollisions, update, pacMen };
+function play() {
+  var audio = document.getElementById("audio");
+  audio.play();
 }
